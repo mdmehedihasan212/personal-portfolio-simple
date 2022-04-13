7022c85f1fdff6e09cb9b58f78919bcfc1ac0961
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-13 06:59:53 +0600

    add install router setup with components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Contact from './Pages/Home/Contact/Contact';
+import Home from './Pages/Home/Home/Home';
+import Projects from './Pages/Home/Projects/Projects';
+import NotFound from './Pages/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
-      <h1>Welcome To My Web</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/Projects' element={<Projects></Projects>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }

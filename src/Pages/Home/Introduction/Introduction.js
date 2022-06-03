c124541d490369459fbe6179d458a5ec2401adc6
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-04 01:58:06 +0600

    optimize navbar

--- src/Pages/Home/Introduction/Introduction.js
+++ src/Pages/Home/Introduction/Introduction.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './Introduction.css';
 import photo from '../../../Assets/profile-pic/profile-pic (46).png';
 
 const Introduction = () => {
     return (
-        <section id='home' className='banner-section d-flex justify-content-center align-items-center p-3 mt-0 text-dark'>
-            <article className='text-section'>
-                <h1>Web developer</h1>
-                <h2>Md Mehedi Hasan</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis necessitatibus provident error nobis voluptates deleniti quidem minima ab ea sit!</p>
-                <button className='contact-btn'>Contact Me</button>
+        <section id='home' className='banner-section d-flex justify-content-around align-items-center p-3 mt-2 mb-5 text-dark'>
+            <article className='text-section w-full mx-auto'>
+                <span className='fs-5 text-uppercase mt-2'>Hi, I'm a Jnr. Web developer</span>
+                <h1 className='fs-1 fw-bolder text-uppercase mt-2'>Md Mehedi Hasan</h1>
+                <p className='fs-4 mt-2'>Barishal, Bangladesh</p>
+                <button className='btn btn-primary px-5 py-3 mt-2'>View My Project</button>
+                <button className='border-0 px-5 py-3 mt-2 mx-4'>Contact Me</button>
             </article>
-            <article className='photo-section'>
-                <img src={photo} alt="img-1" />
+            <article >
+                <img className='w-75 mx-auto d-flex justify-content-center border border-5 rounded-circle' src={photo} alt="img-1" />
             </article>
         </section>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
-import './Header.css';
 
 const Header = () => {
     return (
         <>
-            <Navbar collapseOnSelect sticky='top' className='bg-secondary p-3 text-dark bg-opacity-25'>
+            <Navbar collapseOnSelect sticky='top' expand="lg" className='bg-primary px-5' variant="dark">
                 <Navbar.Brand className='fw-bold fs-2' href="#home">Md Mehedi Hasan</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
@@ -15,7 +14,6 @@ const Header = () => {
                         <Nav.Link href="#services">Services</Nav.Link>
                         <Nav.Link href="#experience">Experience</Nav.Link>
                         <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#family">Family</Nav.Link>
                         <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>

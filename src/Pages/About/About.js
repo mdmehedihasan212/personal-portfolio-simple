f3c8d6b95c8f6674323afd9523150df4228b12de
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-04 21:04:17 +0600

    add project image hover desing

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,29 +1,27 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import { TbCircleDotted } from 'react-icons/tb';
 
 const About = () => {
     return (
-        <section id='about-me'>
-            <article className='text-center'>
-                <div>
-                    <h1 className='mb-5'>About Me</h1>
-                    <p className='w-75 mx-auto mb-4 fs-5'>I'm a web developer with a passion for web design. I enjoy developing simple, clean and slick websites that provide real value to the end user. Thousands of clients have procured exceptional results while working with me. Delivering work within time and budget which meets client;s requirements is our moto.</p>
-                </div>
-            </article>
-            <article className='d-flex justify-content-around align-content-center'>
-                <div style={{ marginLeft: '150px' }}>
-                    <h4> <TbCircleDotted /> Name: Md Mehedi Hasan</h4>
-                    <h4> <TbCircleDotted /> Age: 25 Years</h4>
-                    <h4> <TbCircleDotted /> Nationality: Bangladeshi</h4>
-                    <h4> <TbCircleDotted /> Freelance: Available</h4>
-                </div>
-                <div style={{ marginLeft: '300px' }}>
-                    <h4> <TbCircleDotted /> Address: Barishal Sadar, Bangladesh</h4>
-                    <h4> <TbCircleDotted /> Phone: 01741-445482</h4>
-                    <h4> <TbCircleDotted /> Facebook: mdmehedihasan384</h4>
-                    <h4> <TbCircleDotted /> E-mail: mdmehedihasan384@gmail.com</h4>
-                </div>
-            </article>
+        <section id='about-me' className='px-5 my-5'>
+            <h1 className='text-center'>About Me</h1>
+            <Card className='border-0'>
+                <Card.Body>
+                    <Card.Text className='w-75 mx-auto'>
+                        <h4>I'm a web developer with a passion for web design. I enjoy developing simple,
+                            clean and slick websites that provide real value to the end user. Thousands of clients have procured
+                            exceptional results while working with me. Delivering work within time and budget which meets client;s
+                            requirements is our moto.</h4>
+
+                    </Card.Text>
+                    <Card.Text className='text-center'>
+                        <TbCircleDotted className='ms-2' /> E-mail: mdmehedihasan384@gmail.com
+                        <TbCircleDotted className='ms-2' /> Phone: 01741-445482
+                        <TbCircleDotted className='ms-2' /> Github: https://github.com/mdmehedihasan212
+                    </Card.Text>
+                </Card.Body>
+            </Card>
             <div class="d-flex justify-content-center align-items-center">
                 <button class="btn btn-primary px-5 py-3 mt-2" type="button">Download Resume</button>
             </div>
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,34 +1,59 @@
 import React from 'react';
+import './Projects.css';
 import project from '../../../src/Assets/profile-pic/profile-pic (46).png';
 import { Card, CardGroup } from 'react-bootstrap';
 
 const Projects = () => {
     return (
+        // <div class="container">
+        //     <img src={project} alt="Avatar" class="image" />
+        //     <div class="overlay">
+        //         <div class="text">Hello World</div>
+        //     </div>
+        // </div>
         <section className='my-5 px-5'>
             <h1 className='text-center my-5'>Projects</h1>
             <article>
                 <CardGroup className='gap-4'>
-                    <Card className='border border-primary'>
-                        <Card.Img variant="top" src={project} className='w-50 mx-auto' />
+                    <Card className='border border-primary container'>
+                        <img variant="top" src={project} className='w-50 mx-auto image' alt='img' />
+                        <div className='overlay'>
+                            <p className='text'>Card title</p>
+                        </div>
                     </Card>
-                    <Card className='border border-primary'>
-                        <Card.Img variant="top" src={project} className='w-50 mx-auto' />
+                    <Card className='border border-primary container'>
+                        <img variant="top" src={project} className='w-50 mx-auto image' alt='img' />
+                        <div className='overlay'>
+                            <p className='text'>Card title</p>
+                        </div>
                     </Card>
-                    <Card className='border border-primary'>
-                        <Card.Img variant="top" src={project} className='w-50 mx-auto' />
+                    <Card className='border border-primary container'>
+                        <img variant="top" src={project} className='w-50 mx-auto image' alt='img' />
+                        <div className='overlay'>
+                            <p className='text'>Card title</p>
+                        </div>
                     </Card>
                 </CardGroup>
             </article>
             <article className='mt-4'>
                 <CardGroup className='gap-4'>
-                    <Card className='border border-primary'>
-                        <Card.Img variant="top" src={project} className='w-50 mx-auto' />
+                    <Card className='border border-primary container'>
+                        <img variant="top" src={project} className='w-50 mx-auto image' alt='img' />
+                        <div className='overlay'>
+                            <p className='text'>Card title</p>
+                        </div>
                     </Card>
-                    <Card className='border border-primary'>
-                        <Card.Img variant="top" src={project} className='w-50 mx-auto' />
+                    <Card className='border border-primary container'>
+                        <img variant="top" src={project} className='w-50 mx-auto image' alt='img' />
+                        <div className='overlay'>
+                            <p className='text'>Card title</p>
+                        </div>
                     </Card>
-                    <Card className='border border-primary'>
-                        <Card.Img variant="top" src={project} className='w-50 mx-auto' />
+                    <Card className='border border-primary container'>
+                        <img variant="top" src={project} className='w-50 mx-auto image' alt='img' />
+                        <div className='overlay'>
+                            <p className='text'>Card title</p>
+                        </div>
                     </Card>
                 </CardGroup>
             </article>

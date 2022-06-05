50fa7c3526ad8c466ea34d6ac169e03445e571dd
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-05 19:43:39 +0600

    some correction

--- src/Pages/Home/Introduction/Introduction.js
+++ src/Pages/Home/Introduction/Introduction.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSpring, animated } from 'react-spring'
 import './Introduction.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import photo from '../../../Assets/profile-pic/profile-pic (46).png';
+import { Nav } from 'react-bootstrap';
 
 const Introduction = () => {
+
+    useEffect(() => {
+        AOS.init({ duration: 2000 });
+    }, [])
 
     const [flip, set] = useState(false)
     const props = useSpring({
@@ -17,18 +24,16 @@ const Introduction = () => {
 
     return (
         <section id='home' className='introduction d-flex justify-content-around align-items-center p-3 my-5 text-dark'>
-            <article className='intro-text w-full mx-auto animate__animated animate__slideInLeft animate__slow'>
-                <span className='fs-5 text-uppercase mt-2'>Hi, I'm a Jnr. Web developer</span>
+            <article data-aos="fade-right" className='intro-text w-full mx-auto'>
+                <span className='fs-5 text-uppercase mt-2'>Hi, I'm a Jr. Web developer</span>
                 <animated.h1 style={props} className='fs-1 fw-bolder text-uppercase mt-2'>Md Mehedi Hasan</animated.h1>
-                {/* <h1 className='fs-1 fw-bolder text-uppercase mt-2'>Md Mehedi Hasan</h1> */}
                 <p className='fs-4 mt-2'>Barishal, Bangladesh</p>
-                <div className="d-flex align-items-center justify-content-between">
-                    <button className='btn btn-primary px-5 py-3 mt-2'>View Project</button>
-                    <button className='border-0 px-5 py-3 mt-2'>Contact Me</button>
+                <div className='intro-text w-50'>
+                    <Nav.Link href="#projects" className='btn btn-primary p-2 mt-2 text-white'>Project</Nav.Link>
                 </div>
             </article>
-            <article >
-                <img className='intro-img w-75 mx-auto d-flex justify-content-center border border-5 rounded-circle animate__animated animate__zoomIn animate__slow' src={photo} alt="img-1" />
+            <article data-aos="zoom-in">
+                <img className='intro-img w-75 mx-auto d-flex justify-content-center border border-5 rounded-circle' src={photo} alt="img-1" />
             </article>
         </section>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className='navbar ms-auto text-uppercase'>
                         <Nav.Link href='#home'>Home</Nav.Link>
-                        <Nav.Link href="#about-me">About Us</Nav.Link>
+                        <Nav.Link href="#about-me">About Me</Nav.Link>
                         <Nav.Link href="#skills">Skills</Nav.Link>
                         <Nav.Link href="#projects">Projects</Nav.Link>
                     </Nav>

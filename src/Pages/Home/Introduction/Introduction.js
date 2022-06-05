7e7860c37088468a4947322a17b2245ad41e7d5b
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-06-05 17:07:33 +0600

    add react spring in introduction section

--- src/Pages/Home/Introduction/Introduction.js
+++ src/Pages/Home/Introduction/Introduction.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring'
 import './Introduction.css';
 import photo from '../../../Assets/profile-pic/profile-pic (46).png';
 
 const Introduction = () => {
+
+    const [flip, set] = useState(false)
+    const props = useSpring({
+        to: { opacity: 1 },
+        from: { opacity: 0 },
+        reset: true,
+        reverse: flip,
+        delay: 2000,
+        onRest: () => set(!flip),
+    })
+
     return (
-        // <section id='home' className='container my-5' style={{ width: '100vw' }}>
-        //     <div className="row row-cols-2">
-        //         <article className='col-xl-8'>
-        //             <span className='fs-5 text-uppercase mt-2'>Hi, I'm a Jnr. Web developer</span>
-        //             <h1 className='fs-1 fw-bolder text-uppercase mt-2'>Md Mehedi Hasan</h1>
-        //             <p className='fs-4 mt-2'>Barishal, Bangladesh</p>
-        //             <button className='btn btn-primary px-5 py-3 mt-2'>View My Project</button>
-        //             <button className='border-0 px-5 py-3 mt-2 mx-4'>Contact Me</button>
-        //         </article>
-        //         <article className='col-xl-4 d-sm-inline-block'>
-        //             <img className='img-fluid border border-5 rounded-circle' src={photo} alt="img-1" style={{ width: '300px' }} />
-        //         </article>
-        //     </div>
-        // </section>
         <section id='home' className='introduction d-flex justify-content-around align-items-center p-3 my-5 text-dark'>
             <article className='intro-text w-full mx-auto'>
                 <span className='fs-5 text-uppercase mt-2'>Hi, I'm a Jnr. Web developer</span>
-                <h1 className='fs-1 fw-bolder text-uppercase mt-2'>Md Mehedi Hasan</h1>
+                <animated.h1 style={props} className='fs-1 fw-bolder text-uppercase mt-2'>Md Mehedi Hasan</animated.h1>
+                {/* <h1 className='fs-1 fw-bolder text-uppercase mt-2'>Md Mehedi Hasan</h1> */}
                 <p className='fs-4 mt-2'>Barishal, Bangladesh</p>
                 <div className="d-flex align-items-center justify-content-between">
                     <button className='btn btn-primary px-5 py-3 mt-2'>View Project</button>
